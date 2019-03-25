d7c4c2802f79993945138c7babe76265dc05ee47
Caleb Hester <naclcaleb@gmail.com>
2019-03-25 16:29:46 -0700

    Add api.py for running Flask app in Docker image

---
+++ api.py
@@ -0,0 +1,18 @@
+from flask import Flask, request
+from HLEmail import HLEmail
+
+app = Flask(__name__)
+
+#Instantiate HLEmail
+hlemail = HLEmail("sender@sender.com", "password")
+
+@app.route("/send_html_email", methods=["POST"])
+def send_html_email():
+	hlemail.send_html_email( request.form["email"], request.form["message"] )
+
+@app.route("/send_email", methods=["POST"])
+def send_email():
+	hlemail.send_email( request.form["email"], request.form["message"] )
+
+if __name__ == '__main__':
+	app.run(host="0.0.0.0", port="80")
