fe3a3665d08f1b41ca3f6d7550b1ca4f2d562a3a
Caleb Hester <naclcaleb@gmail.com>
2019-04-03 16:39:47 -0700

    Update api.py to respect password authentication

--- api.py
+++ api.py
@@ -9,11 +9,11 @@ hlemail = HLEmail("email", "password")
 
 @app.route("/send_html_email", methods=["POST"])
 def send_html_email():
-	hlemail.send_html_email( request.form["email"], request.form["message"] )
+	hlemail.send_html_email( request.form["email"], request.form["message"], request.form["password"] )
 
 @app.route("/send_email", methods=["POST"])
 def send_email():
-	hlemail.send_email( request.form["email"], request.form["message"] )
+	hlemail.send_email( request.form["email"], request.form["message"], request.form["password"] )
 
 if __name__ == '__main__':
 	app.run(host="0.0.0.0", port="80")
