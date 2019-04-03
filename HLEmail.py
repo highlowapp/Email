9725e3ce5d89824fd1e7e21689a6cbf618ff6a89
Brayden Morris <34358864+Redemption777@users.noreply.github.com>
2019-04-03 14:56:03 -0700

    Update HLEmail.py

--- HLEmail.py
+++ HLEmail.py
@@ -6,45 +6,55 @@ from email.mime.text import MIMEText
 import html2text
 
 class HLEmail:
-  def __init__(self, sender, password):
+  def __init__(self, sender, password, admin_password):
     self.sender = sender
     self.password = password
+    self.admin_password = admin_password
     
-
+  
   #The function that sends the email
-  def send_email(self, receiver, message): 
-    port = 465  # For SSL
-    smtp_server = "smtp.gmail.com"
-    sender_email = self.sender
-    password = self.password
-    receiver_email = receiver
-    context = ssl.create_default_context()
-    with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
-      server.login(sender_email, password)
-      server.sendmail(sender_email, receiver_email, message.as_string())
+  def HLEmail_send(self, receiver, message): 
+      adminPasswd = "admin"
+      if self.admin_password != adminPasswd:
+        print("Invalid password") #We can add a return function in the future if we want to
+      else:
+        port = 465  # For SSL
+        smtp_server = "smtp.gmail.com"
+        sender_email = self.sender
+        password = self.password
+        receiver_email = receiver
+        context = ssl.create_default_context()
+        with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
+          server.login(sender_email, password)
+          server.sendmail(sender_email, receiver_email, message.as_string())
 
   #The function that adds html to the message
   def send_html_email(self, receiver, message):
-    sender = self.sender
-    password = self.password
-    send = HLEmail(sender, password) 
-    #Fetch the message
-    html_message = ""
-    html_message = message
-    #Create a plaintext version of the HTML email
-    message_plaintext = html2text.html2text( html_message )
-    #Create the MIMETexts
-    mime_message = MIMEMultipart("alternative")
-    message_html = MIMEText( html_message, 'html' )
-    message_plaintext = MIMEText( message_plaintext, 'plain')
+      adminPasswd = "admin"
+      if self.admin_password != adminPasswd:
+        print("Invalid password")
+      else:
+        sender = self.sender
+        password = self.password
+        adminPasswd = self.admin_password
+        send = HLEmail(sender, password, adminPasswd) 
+        #Fetch the message
+        html_message = ""
+        html_message = message
+        #Create a plaintext version of the HTML email
+        #Create the MIMETexts
+        message_plaintext = html2text.html2text( html_message )
+        mime_message = MIMEMultipart("alternative")
+        message_html = MIMEText( html_message, 'html' )
+        message_plaintext = MIMEText( message_plaintext, 'plain')
 
-    #Define email headers
-    mime_message["Subject"] = "HTML/Plaintext email"
-    mime_message["From"] = sender
-    mime_message["To"] = receiver
+        #Define email headers
+        mime_message["Subject"] = "HTML/Plaintext email"
+        mime_message["From"] = sender
+        mime_message["To"] = receiver
 
-    #Attach plaintext and HTML to message
-    mime_message.attach(message_plaintext)
-    mime_message.attach(message_html)
+        #Attach plaintext and HTML to message
+        mime_message.attach(message_plaintext)
+        mime_message.attach(message_html)
 
-    send.send_email(  receiver=receiver,  message=mime_message )
+        send.HLEmail_send(  receiver=receiver,  message=mime_message )
